d7a9af6baea385442a9fffa90f8dd2d3b22c4474
Mario <mariosbpimentel@gmail.com>
2025-10-31 00:18:05 -0300

    update

--- airflow/dags/elt_pipeline.py
+++ airflow/dags/elt_pipeline.py
@@ -1,4 +1,3 @@
-dbt_run = SnowflakeOperator(
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
@@ -6,7 +5,6 @@ from datetime import datetime, timedelta
 import sys
 import os
 
-# Adiciona o root do projeto ao path
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 
 from src.extract.ingest import ingerir_dados
@@ -23,44 +21,45 @@ argumentos_padrao = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag_etl = DAG(
-    'etl_pipeline',
+dag_elt = DAG(
+    'elt_pipeline',
     default_args=argumentos_padrao,
-    description='Pipelina ETL usando Spark e DBT',
+    description='Pipelina ELT',
     schedule_interval='@daily',
     catchup=False
 )
 
-# Tarefa de extração
+#tarefa de extração
 tarefa_extracao = PythonOperator(
     task_id='extracao',
     python_callable=ingerir_dados,
-    dag=dag_etl
+    dag=dag_elt
 )
 
-# Tarefa de transformação
+#tarefa de carga
+tarefa_carga = PythonOperator(
+    task_id='cargaDL',
+    python_callable=carregar_para_snowflake,
+    dag=dag_elt
+)
+
+#tarefa de transformação
 tarefa_transformacao = PythonOperator(
     task_id='transformacao',
     python_callable=transformar_dados,
-    dag=dag_etl
+    dag=dag_elt
 )
 
-# Tarefa de carga
-tarefa_carga = PythonOperator(
-    task_id='carga',
-    python_callable=carregar_para_snowflake,
-    dag=dag_etl
-)
 
-# Tarefa DBT (exemplo usando operator Snowflake - ajuste conforme necessário)
+#tarefa DBT
 tarefa_dbt = SnowflakeOperator(
-    task_id='dbt_run',
-    snowflake_conn_id='snowflake_conn',
-    sql='dbt run',
-    warehouse='COMPUTE_WH',
-    database='YOUR_DATABASE',
-    dag=dag_etl
+    task_id='',
+    snowflake_conn_id='',
+    sql='',
+    warehouse='',
+    database='',
+    dag=dag_elt
 )
 
-# Dependências
-tarefa_extracao >> tarefa_transformacao >> tarefa_carga >> tarefa_dbt
+#dependências
+tarefa_extracao >> tarefa_carga >> tarefa_transformacao >> tarefa_dbt
